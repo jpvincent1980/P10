ea827aa6bb5ac002ebe6a4361f606f280fa6d84f
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2022-03-17 13:47:35 +0100

    rest_framework login template replaced with customized one

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views
 
 from .views import SignupView
 
 app_name = "accounts"
 
 urlpatterns = [
+    path('login/', views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('signup/', SignupView.as_view(template_name="accounts/signup.html"),
          name="signup"),
 ]
