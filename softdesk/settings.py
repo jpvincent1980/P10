793d39c1d77e4dee17023ba890aba51aae52e6ef
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2022-03-17 12:17:16 +0100

    settings.py update

--- softdesk/settings.py
+++ softdesk/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'softdesk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "accounts"],
+        'DIRS': [os.path.join(BASE_DIR, "accounts")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'softdesk.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
